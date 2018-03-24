78e7e8b75692be3cbf1f1308c619c67bca6305e8
Achyut Dev <achyut.dev@gmail.com>
2018-03-24 10:46:18 -0400

    Revision - video background added

--- resource/js/main.js
+++ resource/js/main.js
@@ -1,15 +1,6 @@
 /**
  * Created by achyutdev on 5/20/17.
  */
-function showKota() {
-    $(this).innerHTML("kota");
-}
-
-
-$(document).ready(function(){
-    $("#hideKota").mouseover(showKota());
-});
-
 
 $( document ).ready(function() {
 
